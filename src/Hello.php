ef012368224ca0d825bd04c5093f9203d461fb59
Igor Popov <ipkroks@gmail.com>
2021-04-16 02:06:42 +0300

    fix src/Hello.php by phpcs

--- src/Hello.php
+++ src/Hello.php
@@ -2,14 +2,15 @@
 
 namespace popov\hello;
 
-class Hello {
-	public function getMessage($message)
-	{
-		return "Hello " . $message;
-	}
+class Hello
+{
+    public function getMessage($message)
+    {
+        return "Hello " . $message;
+    }
 
-	public function getSum($one, $two)
-	{
-		return $one;
-	}
+    public function getSum($one, $two)
+    {
+        return $one;
+    }
 }
